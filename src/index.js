127eb097e70bb4bb4c4ca19c8d391e568820a4fd
Даниил <deadxraver@yandex.ru>
2024-12-11 18:20:17 +0300

    РАЗБИЛ на модули наши компонентики

--- src/index.js
+++ src/index.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
 import {useTimer} from './timer';
 import PhysicsVisualization from "./visualization/drawer";
 import Scales from "./scales";
 import Inventory from "./inventory";
+import Form from "./form";
 
-function App() {
+function ScalesComponent() {
+	const [selectedObject, setSelectedObject] = useState(null);
+	const [objectOnScales, setObjectOnScales] = useState(null);
+	return (
+		<div style={{display: 'flex', justifyContent: 'space-around'}}>
+			<Inventory onSelect={setSelectedObject}/>
+			<Scales selectedObject={objectOnScales} onPlace={setObjectOnScales} selectedObjectState={selectedObject}
+					setSelectedObject={setSelectedObject}/>
+		</div>
+	)
+}
+
+function TimerComponent() {
 	const {time, isRunning, startTimer, stopTimer} = useTimer();
-
 	return (
 		<>
-			<Scales/>
-			<Inventory/>
-			<PhysicsVisualization/>
 			<button onClick={startTimer} disabled={isRunning}>Пуск</button>
 			<button onClick={stopTimer} disabled={!isRunning}>Стоп</button>
 			<div className="timer-container">
 				<div className="timer">{time}</div>
 			</div>
+		</>
+	)
+}
+
+function App() {
+	return (
+		<>
+			<ScalesComponent/>
+			<PhysicsVisualization/>
+			<TimerComponent/>
+			<Form/>
 		</>
 	)
 }
